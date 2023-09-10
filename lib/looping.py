ebd9cbe1352f57de81d0dbb2f865225bc873d0b9
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-09-10 22:26:43 +0300

    looping complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+       count = 10
+       while count > 0:
+            print(count)
+            count -= 1
+       print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+     squared_lst = [num ** 2 for num in int_list if isinstance(num, int)]
+     return squared_lst
 
 def fizzbuzz():
     # code goes here!
-    pass
+     for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
